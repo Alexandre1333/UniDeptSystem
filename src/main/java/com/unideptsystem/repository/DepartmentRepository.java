756e78f0062137df606860e2a657a3421c4428ed
Alexandre Labranche <alexandre.labranche@atnexus.ca>
2025-11-21 20:25:37 -0500

    Small refactoring

--- src/main/java/com/unideptsystem/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unideptsystem.repository;
-
-import com.unideptsystem.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    Optional<Department> findByName(String name);
-}
